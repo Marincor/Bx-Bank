9a26ed974545578beb58d1e93820b1e13d1ead14
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-06-30 17:17:33 -0300

    teste de script.

---
+++ assets/JS/Formulario.js
@@ -0,0 +1,17 @@
+var cpf = document.getElementById("cpf");
+var nDaConta = document.getElementById("n-da-conta");
+var botao = document.getElementById("botao-entrar");
+
+botao.onclick ( valida ()) ;
+
+function valida () {
+
+    if (cpf.value == 37332617803) {
+
+        alert ("Acertou")
+    }
+    
+
+
+
+}
