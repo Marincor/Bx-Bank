d7a6cc25b52a67cb106f9fbf1d86118d6c1bf610
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-07-02 18:58:42 -0300

    arquivos js iniciais funcionando para validação; faltando informar situação esperada para a condição true

--- assets/JS/Formulario.js
+++ assets/JS/Formulario.js
@@ -1,17 +1,17 @@
-var cpf = document.getElementById("cpf");
-var nDaConta = document.getElementById("n-da-conta");
-var botao = document.getElementById("botao-entrar");
-
-botao.onclick ( valida ()) ;
-
-function valida () {
-
-    if (cpf.value == 37332617803) {
-
-        alert ("Acertou")
-    }
-    
+import { botaoClique } from "./Validador.js";
 
 
 
-}
+
+
+const botao = document.querySelector ('[data-formulario__botao]');
+
+ botao.addEventListener ('click', botaoClique)
+
+
+ 
+  
+
+
+
+ 
